5876a71d20adec0203bfaaaf35f69200cbdffcfa
Aariyan007 <aariyansunu28@gmail.com>
2026-02-09 01:11:38 +0530

    added copyright

--- mitzegle/src/app/components/Footer.tsx
+++ mitzegle/src/app/components/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='relative z-10 text-center py-6 text-xs text-zinc-500 bg-black border-b border-white/10'>
+      {new Date().getFullYear()} &copy; MITZEGLE. All rights reserved.
+    </div>
   )
 }
 
--- mitzegle/src/app/page.tsx
+++ mitzegle/src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { motion } from "framer-motion";
 
@@ -183,6 +184,7 @@ export default function Home() {
           </div>
         </div>
       </main>
+      <Footer/>
     </>
   );
 }
